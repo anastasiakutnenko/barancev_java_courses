729001f6446d6548e01f0f14ce34b92c875f4733
Anastasia <anastasya.kutnenko@gmail.com>
2019-02-13 00:09:17 +0200

    task 1 - added class and method for calculation distance between 2 points

--- sandbox/src/main/java/mypackage/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/mypackage/sandbox/MyFirstProgram.java
@@ -1,7 +1,11 @@
 package mypackage.sandbox;
 
-public class MyFirstProgram{
+public class MyFirstProgram {
 	public static void main(String[] args) {
-	System.out.println("Hello world");
+		Point p1 = new Point(-1, 2);
+		Point p2 = new Point(3, 2);
+		System.out.println("Distance between points (" + p1.x + "; " + p1.y + ") and (" + p2.x + "; " + p2.y + ") = " + p1.distance(p2));
+	}
+
 }
-}
+
