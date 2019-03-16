e6053fb3ea262ef1104956499e1a93d541ffb2ac
Anastasia <anastasya.kutnenko@gmail.com>
2019-03-16 13:43:28 +0200

    committed GroupHelper class

--- addressbook-web-tests/src/test/java/mypackage/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/mypackage/addressbook/appmanager/GroupHelper.java
@@ -4,7 +4,6 @@ import mypackage.addressbook.model.GroupData;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-
 public class GroupHelper extends HelperBase {
 
     public GroupHelper(WebDriver wd) {
