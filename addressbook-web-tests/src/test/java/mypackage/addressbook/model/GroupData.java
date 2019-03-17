ac1daafa260dd771fa5f84b942944993f89863cb
Anastasia <anastasya.kutnenko@gmail.com>
2019-03-17 23:25:51 +0200

    added Asserts to compare elements in GroupCreationTests, with searching max id

--- addressbook-web-tests/src/test/java/mypackage/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/mypackage/addressbook/model/GroupData.java
@@ -3,7 +3,11 @@ package mypackage.addressbook.model;
 import java.util.Objects;
 
 public class GroupData {
-    private final String id;
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    private int id;
     private final String name;
     private final String header;
     private final String footer;
@@ -16,21 +20,9 @@ public class GroupData {
                 '}';
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        GroupData groupData = (GroupData) o;
-        return Objects.equals(id, groupData.id) &&
-                Objects.equals(name, groupData.name);
-    }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name);
-    }
 
-    public GroupData(String id, String name, String header, String footer) {
+    public GroupData(int id, String name, String header, String footer) {
         this.id = id;
         this.name = name;
         this.header = header;
@@ -38,13 +30,13 @@ public class GroupData {
     }
 
     public GroupData(String name, String header, String footer) {
-        this.id = null;
+        this.id = 0;
         this.name = name;
         this.header = header;
         this.footer = footer;
     }
 
-    public String getId() { return id; }
+    public int getId() { return id; }
 
     public String getName() {
         return name;
@@ -56,5 +48,19 @@ public class GroupData {
 
     public String getFooter() {
         return footer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GroupData groupData = (GroupData) o;
+        return id == groupData.id &&
+                Objects.equals(name, groupData.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
     }
 }
