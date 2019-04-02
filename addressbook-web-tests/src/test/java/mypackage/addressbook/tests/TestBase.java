0e2d834bc1b5d9a408953537c3fdb20e04b2d34c
Anastasia <anastasya.kutnenko@gmail.com>
2019-04-03 00:47:48 +0300

    added logger

--- addressbook-web-tests/src/test/java/mypackage/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/mypackage/addressbook/tests/TestBase.java
@@ -2,10 +2,18 @@ package mypackage.addressbook.tests;
 
 import mypackage.addressbook.appmanager.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class TestBase {
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     protected final static ApplicationManager app
             = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
@@ -19,5 +27,16 @@ public class TestBase {
     public void tearDown() throws Exception {
         app.stop();
     }
+
+    @BeforeMethod
+    public void logTestStart(Method m, Object[] p) {
+        logger.info("start test" + m.getName() + " with parameters " + Arrays.asList(p));
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void logTestStop(Method m) {
+        logger.info("stop test" + m.getName());
+    }
+
 
 }
